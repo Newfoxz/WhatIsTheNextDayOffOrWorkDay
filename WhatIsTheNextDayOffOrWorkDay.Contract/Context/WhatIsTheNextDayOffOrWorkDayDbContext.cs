d54dac76b71e38989846eac569892bc3b3b84a37
felipe.guisouza54 <felipe.guisouza54@gmail.com>
2020-11-10 20:43:19 -0300

    dbcontext DONE

---
+++ WhatIsTheNextDayOffOrWorkDay.Contract/Context/WhatIsTheNextDayOffOrWorkDayDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatIsTheNextDayOffOrWorkDay.Domain.Entity;
+
+namespace WhatIsTheNextDayOffOrWorkDay.Repository.Context
+{
+    public class WhatIsTheNextDayOffOrWorkDayDbContext : DbContext
+    {
+        public WhatIsTheNextDayOffOrWorkDayDbContext(DbContextOptions options) 
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Escala> Escalas { get; set; }
+        public DbSet<Sequencia> Sequencias { get; set; }
+    }
+}
