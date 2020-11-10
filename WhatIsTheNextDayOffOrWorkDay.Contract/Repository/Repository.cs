8d88d3d38ec6c43efd2eb12bbcca442154d6f82b
felipe.guisouza54 <felipe.guisouza54@gmail.com>
2020-11-10 20:37:14 -0300

    repository and entity framework DONE

---
+++ WhatIsTheNextDayOffOrWorkDay.Contract/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatIsTheNextDayOffOrWorkDay.Domain.Contract;
+
+namespace WhatIsTheNextDayOffOrWorkDay.Repository.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByFilter(Predicate<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<T> GetManyByFilter(Predicate<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WhatIsTheNextDayOffOrWorkDay.Contract/Repository/RepositorySequencia.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatIsTheNextDayOffOrWorkDay.Domain.Contract;
+using WhatIsTheNextDayOffOrWorkDay.Domain.Entity;
+
+namespace WhatIsTheNextDayOffOrWorkDay.Repository.Repository
+{
+    public class RepositorySequencia : Repository<Sequencia>, IRepositorySequencia
+    {
+
+    }
+}
