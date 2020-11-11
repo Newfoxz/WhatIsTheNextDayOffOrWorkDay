84598c3a0240576a09c30ed61ec5545269bffab0
felipe.guisouza54 <felipe.guisouza54@gmail.com>
2020-11-11 18:14:55 -0300

    migration DONE

---
+++ WhatIsTheNextDayOffOrWorkDay.Contract/Migrations/20201111210113_1.0-version_db.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WhatIsTheNextDayOffOrWorkDay.Repository.Migrations
+{
+    public partial class _10version_db : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Escalas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descricao = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    VigenciaInicial = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Escalas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Apelido = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    EscalaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pessoas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pessoas_Escalas_EscalaId",
+                        column: x => x.EscalaId,
+                        principalTable: "Escalas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sequencias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numero = table.Column<int>(type: "int", nullable: false),
+                    Indicador = table.Column<int>(type: "int", nullable: false),
+                    EscalaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sequencias", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sequencias_Escalas_EscalaId",
+                        column: x => x.EscalaId,
+                        principalTable: "Escalas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pessoas_EscalaId",
+                table: "Pessoas",
+                column: "EscalaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sequencias_EscalaId",
+                table: "Sequencias",
+                column: "EscalaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pessoas");
+
+            migrationBuilder.DropTable(
+                name: "Sequencias");
+
+            migrationBuilder.DropTable(
+                name: "Escalas");
+        }
+    }
+}
