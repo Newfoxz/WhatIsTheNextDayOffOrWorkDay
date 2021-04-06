40c7b8e290361b7a552a5f4047aea4dd7be3b313
felipe.guisouza54 <felipe.guisouza54@gmail.com>
2021-04-05 22:01:58 -0300

    route storage

--- WhatIsTheNextDayOffOrWorkDay.Web/ClientApp/src/app/app.module.ts
+++ WhatIsTheNextDayOffOrWorkDay.Web/ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { RouteStorage } from './security/route.storage';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'fetch-data', component: FetchDataComponent, canActivate: [RouteStorage] },
     ])
   ],
   providers: [],
